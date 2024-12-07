eadca7f9337a754d76926fd3a82efda1044c5c9b
ccauvang <fakeonthehat@gmail.com>
2024-12-07 18:42:36 +0700

    New: Create setprefix cmd to set bot prefix in differnt guild, Create new seting command file for seting user dank work message catch.

---
+++ commands/setting/setprefix.js
@@ -0,0 +1,157 @@
+const { EmbedBuilder, ButtonBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, } = require('@discordjs/builders');
+const { PermissionsBitField, MessageFlags, TextInputStyle, ComponentType } = require('discord.js');
+const { setLocale, ie } = require('../../util/i18n');
+const { QuickDB } = require('quick.db');
+const db = new QuickDB({ filePath: "database/main.sqlite" });
+require('dotenv').config();
+
+module.exports = {
+    name: 'setprefix',
+    aliases: ['prefix', 'pre'],
+    description: 'setting.setprefix.description',
+    cooldown: 5,
+    category: __dirname.split('\\').pop(),
+    usage: '^setprefix',
+    async run(message, lang) {
+
+        setLocale(lang);
+
+        if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            const nonPremission = new EmbedBuilder()
+                .setTitle(ie.__(`${this.category}.${this.name}.nonPremission.title`))
+                .setDescription(ie.__(`${this.category}.${this.name}.nonPremission.description`))
+                .setColor(0xFF0000);
+            message.channel.send({ embeds: [nonPremission] }).then(msg => setTimeout(() => msg.delete().catch(console.error), 15 * 1e3));
+            return;
+        };
+
+        const setPrefixButton = new ButtonBuilder()
+            .setCustomId('setPreBtn')
+            .setLabel('Set prefix')
+            .setStyle('Success');
+
+        const resetPrefixButton = new ButtonBuilder()
+            .setCustomId('resetPreBtn')
+            .setLabel('Reset prefix')
+            .setStyle('Primary');
+
+        const setPrefixButtonRow = new ActionRowBuilder()
+            .addComponents(setPrefixButton);
+
+        const currentPrefix = await db.get(`Guild._${message.guild.id}.prefix`);
+
+        if (currentPrefix != null && currentPrefix != process.env.PREFIX) {
+            setPrefixButtonRow.addComponents(resetPrefixButton);
+        };
+
+        const setPrefixInput = new TextInputBuilder()
+            .setCustomId('setPreInp')
+            .setLabel('Prefix:')
+            .setPlaceholder('ex: #.')
+            .setStyle(TextInputStyle.Short)
+            .setMaxLength(3)
+            .setMinLength(1)
+            .setRequired(true);
+
+        const setPrefixModal = new ModalBuilder()
+            .setCustomId(`setPrefixModal_${message.author.id}`)
+            .setTitle('Prefix you want to set for this server.')
+            .setComponents(
+                new ActionRowBuilder().addComponents(setPrefixInput)
+            );
+
+        // await db.set(`Guild._${message.guild.id}.prefix`, prefixWanToSet);
+        const setPrefixCard = new EmbedBuilder()
+            .setTitle(ie.__(`${this.category}.${this.name}.setPrefixCard.preview.title`))
+            .setDescription(ie.__mf(`${this.category}.${this.name}.setPrefixCard.preview.description`, { currentPrefix: currentPrefix }))
+            .setColor(0x00FF80)
+            .setFooter({ text: ie.__mf(`${this.category}.${this.name}.setPrefixCard.preview.footer`, { tag: message.author.tag, }), iconURL: message.author.avatarURL() })
+            .setTimestamp();
+
+        const setPrefixMessage = await message.channel.send({ embeds: [setPrefixCard], components: [setPrefixButtonRow] });
+
+        function filter(i) {
+            return message.author.id == i.user.id
+        };
+
+        const collector = setPrefixMessage.createMessageComponentCollector({
+            // filter,
+            componentType: ComponentType.Button,
+            time: 120 * 1e3
+        });
+
+        var canAddAwaitModal = true;
+        var endCollector = false;
+
+
+
+        collector.on('collect', async (buttonInteraction) => {
+            if (!filter(buttonInteraction)) {
+                buttonInteraction.reply({ content: `This isn't your bro!`, flags: MessageFlags.Ephemeral })
+                return;
+            };
+            console.log('Im hare');
+
+            if (buttonInteraction.customId == 'setPreBtn') {
+                console.log('i got clicked');
+                await buttonInteraction.showModal(setPrefixModal);
+                if (canAddAwaitModal) {
+                    canAddAwaitModal = false;
+                    console.log('Add new modal');
+
+                    await buttonInteraction.awaitModalSubmit({ filter, time: 120 * 1e3 })
+                        .then(async (modalInteraction) => {
+                            if (endCollector == false) {
+                                const prefixRecive = await modalInteraction.fields.getTextInputValue('setPreInp');
+                                await db.set(`Guild._${modalInteraction.guildId}.prefix`, prefixRecive);
+
+                                setPrefixCard.setTitle(ie.__(`${this.category}.${this.name}.setPrefixCard.success.title`))
+                                setPrefixCard.setDescription(ie.__mf(`${this.category}.${this.name}.setPrefixCard.success.description`, { prefix: prefixRecive }))
+                                setPrefixCard.setFooter({
+                                    text: ie.__mf(`${this.category}.${this.name}.setPrefixCard.success.footer`, {
+                                        tag: modalInteraction.user.tag,
+                                    }),
+                                    iconURL: modalInteraction.user.avatarURL()
+                                })
+
+                                await modalInteraction.update({ embeds: [setPrefixCard], components: [] });
+
+                                collector.resetTimer({ time: 60 * 1e3 });
+                            } else {
+                                buttonInteraction.reply({ content: 'yooo what do you find bro.', flags: MessageFlags.Ephemeral })
+                            };
+                        })
+                        .catch(err => {
+                            // console.log(err);
+                            collector.stop();
+                        });
+                };
+                return;
+            };
+
+            if (buttonInteraction.customId == 'resetPreBtn') {
+
+                const ressetPrefixCard = new EmbedBuilder()
+                    .setTitle(ie.__(`${this.category}.${this.name}.resetPrefixCard.title`))
+                    .setDescription(ie.__mf(`${this.category}.${this.name}.resetPrefixCard.description`, { defaultPrefix: process.env.PREFIX }))
+                    .setColor(0x00FF80)
+                    .setFooter({ text: ie.__mf(`${this.category}.${this.name}.resetPrefixCard.footer`, { tag: message.author.tag, }), iconURL: message.author.avatarURL() })
+                    .setTimestamp();
+
+                await buttonInteraction.update({ embeds: [ressetPrefixCard], components: [] });
+
+                await db.set(`Guild._${buttonInteraction.guildId}.prefix`, process.env.PREFIX)
+                collector.resetTimer({ time: 60 * 1e3 });
+                return;
+            }
+        });
+
+        collector.on('end', async () => {
+            endCollector = true;
+            setTimeout(() => {
+                setPrefixMessage.delete();
+            }, 3 * 1e3)
+        });
+
+    }
+};
---
+++ commands/setting/setworkcatch.js
@@ -0,0 +1,59 @@
+const { EmbedBuilder } = require('@discordjs/builders');
+const { setLocale, ie } = require('../../util/i18n');
+const { QuickDB } = require('quick.db');
+const db = new QuickDB({ filePath: 'database/main.sqlite' });
+
+module.exports = {
+    name: 'setworkcatch',
+    description: 'setting.setworkcatch.description',
+    aliases: ['swc', 'wc'],
+    cooldown: 5,
+    category: __dirname.split('\\').pop(),
+    usage: '^setworkcatch or ^swc',
+    async run(message, lang) {
+        setLocale(lang);
+
+        const userStatus = await db.get(`User._${message.author.id}.catchDankMsg`);
+
+        if (userStatus == null) {
+            await db.set(`User.user_${message.author.id}.catchDankMsg`, 1);
+
+            const newUserCard = new EmbedBuilder()
+                .setTitle(ie.__(`${this.category}.${this.name}.newUserCard.title`))
+                .setDescription(ie.__(`${this.category}.${this.name}.newUserCard.description`))
+                .setColor(0x00FF80)
+                .setTimestamp();
+
+            message.channel.send({ embeds: [newUserCard] }).then(msg => {
+                setTimeout(() => {
+                    msg.delete().catch(console.error);
+                }, 30 * 1e3);
+            });
+            return;
+        };
+
+        const settingCard = new EmbedBuilder()
+            .setTitle(ie.__mf(`${this.category}.${this.name}.settingCard.title`, { status: userStatus == 1 ? 'OFF' : 'ON' }))
+            .setTimestamp();
+
+        if (userStatus) {
+            await db.set(`User._${message.author.id}.catchDankMsg`, 0);
+            settingCard.setColor(0xFF0000)
+            message.channel.send({ embeds: [settingCard] }).then(msg => {
+                setTimeout(() => {
+                    msg.delete().catch(console.error);
+                }, 30 * 1e3);
+            });
+            return;
+        } else {
+            await db.set(`User._${message.author.id}.catchDankMsg`, 1);
+            settingCard.setColor(0x00FF80)
+            message.channel.send({ embeds: [settingCard] }).then(msg => {
+                setTimeout(() => {
+                    msg.delete().catch(console.error);
+                }, 30 * 1e3);
+            });
+            return;
+        };
+    }
+};
