a635f021800e229b5f8a17f1d5691eb2c993c598
ccauvang <fakeonthehat@gmail.com>
2024-12-07 18:38:53 +0700

    New: Create new command help to help user use the bot.

---
+++ commands/info/help.js
@@ -0,0 +1,76 @@
+const { EmbedBuilder } = require('@discordjs/builders');
+const { setLocale, ie } = require('../../util/i18n');
+const { QuickDB } = require('quick.db');
+const db = new QuickDB({ filePath: 'database/main.sqlite' });
+
+module.exports = {
+    name: 'help',
+    description: 'info.help.description',
+    aliases: ['h'],
+    cooldown: 5,
+    category: __dirname.split('\\').pop(),
+    usage: '^help or ^h',
+    async run(message, lang, args) {
+        setLocale(lang);
+
+        var prefixServer = await db.get(`Guild._${message.guild.id}.prefix`);
+        if (prefixServer == null) prefixServer = 'd.';
+
+        const client = message.client;
+
+        if (!args[0]) {
+
+            const helpCard = new EmbedBuilder()
+                .setTitle(ie.__(`${this.category}.${this.name}.helpCard.title`))
+                .setDescription(ie.__(`${this.category}.${this.name}.helpCard.description`))
+                .setColor(0x00FF80)
+                .setFooter({ text: ie.__(`${this.category}.${this.name}.helpCard.footer`).replace(/[\^]/g, prefixServer) })
+                .setTimestamp();
+
+            const categorys = client.categorys.keys();
+            for (ctg of categorys) {
+                const commands = client.categorys.get(ctg);
+                const commandFields = commands.join(', ');
+                helpCard.addFields({
+                    name: ctg, value: commandFields, inline: true
+                });
+            };
+            message.channel.send({ embeds: [helpCard] }).then(msg => {
+                setTimeout(() => {
+                    msg.delete().catch(console.error);
+                }, 90 * 1e3);
+            });
+            return;
+        };
+
+        const command = client.commands.get(args[0]) || client.commands.get(client.aliases.get(args[0]));
+
+        if (command) {
+
+            const helpCmdCard = new EmbedBuilder()
+                .setTitle(ie.__(`${this.category}.${this.name}.helpCmdCard.title`))
+                .setDescription(ie.__(`${this.category}.${this.name}.helpCmdCard.description`))
+                .addFields({
+                    name: ie.__mf(`${this.category}.${this.name}.helpCmdCard.fields.name`, { name: command.name }),
+                    value: ie.__mf(`${this.category}.${this.name}.helpCmdCard.fields.value`, {
+                        aliases: command.aliases.join(', '),
+                        description: ie.__(command.description),
+                        cooldown: command.cooldown,
+                        category: command.category,
+                        usage: command.usage.replace(/[\^]/g, prefixServer)
+                    }),
+                    inline: true
+                })
+                .setColor(0x00FF80)
+                .setFooter({ text: ie.__(`${this.category}.${this.name}.helpCmdCard.footer`).replace(/[\^]/g, prefixServer) })
+                .setTimestamp();
+
+            message.channel.send({ embeds: [helpCmdCard] }).then(msg => {
+                setTimeout(() => {
+                    msg.delete().catch(console.error);
+                }, 60 * 1e3);
+            });
+            return;
+        };
+    }
+};
