d095d8126d86e14ba83c1c06a47bda4dd288d35e
ccauvang <fakeonthehat@gmail.com>
2025-12-20 17:06:46 +0700

    feet: Now also catch prefix command of dank bot.

--- workprocess/processWorkMgs.js
+++ workprocess/processWorkMgs.js
@@ -2,33 +2,61 @@ const { QuickDB } = require('quick.db');
 const db = new QuickDB({ filePath: 'database/main.sqlite' });
 require('dotenv').config();
 
+/** 
+*@global 
+*@param {object} message - The message of the Dank bot.
+*@param {string} IDUser - The ID of the user use interaction or prefix.
+*/
+
 async function workProcessMgs(message) {
     if (!message) return;
     if (message.author.id != process.env.IDBOTDISCORD) return;
-    if (message.interaction == null) return;
     if (message.embeds.length < 1) return;
 
-    const userCatchStatus = await db.get(`User._${message.interactionMetadata.user.id}.catchDankMsg`);
-    if (userCatchStatus == null || userCatchStatus == 0) return;
+    /** 
+     *@param {object} message - The message of the Dank bot.
+     *@param {string} IDUser - The ID of the user use interaction or prefix.
+     *@param {string} commandName - Name of the work command.
+     */
 
-    const embedOfMessage = message.embeds[0];
-    const msgRootInteract = message.interaction;
-    const nameCommand = 'work shift';
+    async function getInfoUserAndSendTheHelpMessage(message, IDUser, commandName) {
+        const nameCommand = 'work shift';
+        if (commandName == nameCommand) {
+            const userCatchStatus = await db.get(`User._${IDUser}.catchDankMsg`);
+            if (userCatchStatus == null || userCatchStatus == 0) return;
+            const embedOfMessage = message.embeds[0];
+            if (embedOfMessage.toString().match('the ball!') != null) return;
+            // const msgRootInteract = message.interaction;
 
-    console.log(embedOfMessage);
-    console.log(msgRootInteract);
+            console.log(embedOfMessage);
+            // console.log(msgRootInteract);
 
 
-    if (msgRootInteract.commandName == nameCommand) {
-        await message.react('1313857088245731359');
-        await message.channel.send({ embeds: [embedOfMessage] }).then(msg => {
-            if (msg.deletable) {
-                setTimeout(() => {
-                    message.reactions.removeAll().catch(console.error);
-                    msg.delete().catch(console.error);
-                }, 30 * 1e3);
-            };
-        });
+            await message.react('1313857088245731359');
+            await message.channel.send({ embeds: [embedOfMessage] }).then(msg => {
+                if (msg.deletable) {
+                    setTimeout(() => {
+                        message.reactions.removeAll().catch(console.error);
+                        msg.delete().catch(console.error);
+                    }, 30 * 1e3);
+                };
+            });
+        };
+    }
+
+    if (message.interaction != null) {
+        const useID = message.interactionMetadata.user.id;
+        const commandName = message.interaction.commandName;
+
+        await getInfoUserAndSendTheHelpMessage(message, useID, commandName);
+    } else {
+        const useID = message.mentions.repliedUser.id;
+        const repliedMessage = await message.channel.messages.fetch(message.reference.messageId);
+        var commandName = repliedMessage.content.match(/work shift/g);
+        if (commandName != null) commandName = commandName[0];
+
+
+        await getInfoUserAndSendTheHelpMessage(message, useID, commandName);
     };
 };
 
